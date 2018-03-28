35f44f1ddcbae5344e982127c926f76285ce5473
b1f6c1c4 <b1f6c1c4@gmail.com>
2018-03-28 14:42:13 +0800

    Token

--- index.js
+++ index.js
@@ -4,7 +4,18 @@ const fs = require('fs');
 const debug = require('debug')('hub-sync');
 const HubSync = require('./sync');
 
-const readToken = () => fs.readFileSync(path.join(os.homedir(), '.hub-sync'), 'utf-8').trim();
+const readToken = ({ token, tokenFile }) => {
+  if (token) {
+    return token;
+  }
+  try {
+    return fs.readFileSync(tokenFile, 'utf-8').trim();
+  } catch (e) {
+    console.error(`Error occured during reading token file ${tokenFile}.`);
+    console.error('Please check its existance and permission.');
+    return undefined;
+  }
+}
 
 const parseSpec = (spec = '', omitUser = false) => {
   const sp = spec === '' ? [] : spec.split('/', 3);
@@ -22,12 +33,11 @@ const parseSpec = (spec = '', omitUser = false) => {
 
 const show = ({ user, repo, branch }) => `https://github.com/${user}/${repo}/tree/${branch}`;
 
-const runUpdate = async ({ what, from, force, dryRun }) => {
+const runUpdate = async ({ what, from, force, dryRun }, token) => {
   debug({ what, from, force, dryRun });
   what = parseSpec(what, true);
   from = parseSpec(from);
   debug({ what, from });
-  const token = readToken();
   const hs = new HubSync({ token });
   const cfg = await hs.fill({ what, from, force })
   console.log(`Will update ${show(cfg.what)} <- ${show(cfg.from)} (force: ${force})`);
@@ -43,6 +53,16 @@ const runUpdate = async ({ what, from, force, dryRun }) => {
 }
 
 require('yargs')
+  .option('token-file', {
+    describe: 'Github token file, see https://github.com/settings/tokens',
+    default: path.join(os.homedir(), '.hub-sync'),
+    type: 'string',
+  })
+  .option('t', {
+    alias: 'token',
+    describe: 'Github token, see https://github.com/settings/tokens',
+    type: 'string',
+  })
   .command(['update <what> [<from>]', '$0'], 'Update github repo', (yargs) => {
     yargs
       .option('f', {
@@ -65,7 +85,11 @@ require('yargs')
         type: 'string',
       });
   }, (argv) => {
-    runUpdate(argv).catch((e) => {
+    const token = readToken(argv);
+    if (!token) {
+      return;
+    }
+    runUpdate(argv, token).catch((e) => {
       console.error(e.message);
       if (e.response) {
         console.error(e.response.data);
