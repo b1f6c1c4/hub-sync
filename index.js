b8f7ebbbc9b31dca4c52b466b7ead12086d81677
b1f6c1c4 <b1f6c1c4@gmail.com>
2020-07-12 21:16:13 -0400

    parse server response

--- index.js
+++ index.js
@@ -73,31 +73,95 @@ const runUpdate = async ({ what, from, force, create, delete: del, dryRun }, tok
   debug({ what, from });
   const hs = new HubSync({ token });
   const cfg = await hs.fill({ what, from, force })
+  const forceful = force ? chalk`{red {bold FORCED}}` : chalk`{dim (fast forward only)}`;
   if (create) {
-    console.log(`Will create ${show(cfg.what)} <- ${show(cfg.from)} (force: ${force})`);
+    console.error(chalk`{green Will create ${show(cfg.what)}} ${forceful}`);
+    console.error(chalk`{magenta ^ from upstream ${show(cfg.from)}}`);
   } else if (del) {
-    console.log(`Will delete ${show(cfg.what)}`);
+    console.error(chalk`{red Will delete ${show(cfg.what)}}`);
   } else {
-    console.log(`Will update ${show(cfg.what)} <- ${show(cfg.from)} (force: ${force})`);
+    console.error(chalk`{cyan Will update ${show(cfg.what)}} ${forceful}`);
+    console.error(chalk`{magenta ^ from upstream ${show(cfg.from)}}`);
   }
-  const sha = await hs.getRefs(del ? cfg.what : cfg.from);
-  if (del) {
-    console.log(`Got origin head sha: ${sha}`);
-  } else {
-    console.log(`Got upstream head sha: ${sha}`);
-  }
-  if (dryRun) {
-    console.log('Stopped because --dry-run');
-    return;
-  }
-  if (del) {
-    const res = await hs.delRefs(cfg.what);
-    console.log(`Succeed, ${show(cfg.what)} is deleted`);
-    debug(res);
-  } else {
-    const res = await hs.setRefs(cfg.what, sha, { force, create });
-    console.log(`Succeed, ${show(cfg.what)} is at ${res.object.sha}`);
-    debug(res);
+  try {
+    const old = await hs.getRefs(cfg.what);
+    const sha = !del && await hs.getRefs(cfg.from);
+    if (del) {
+      console.error(chalk`Got origin head SHA-1: {yellow ${old}}`);
+    } else {
+      console.error(chalk`Got origin head SHA-1: {yellow ${old}}`);
+      console.error(chalk`Got upstream head SHA-1: {yellow ${sha}}`);
+      if (old === sha) {
+        console.error(chalk`{greenBright Skipped:} {yellow ${show(cfg.what)}} is already in sync.`);
+        return true;
+      }
+    }
+    if (!create && force) {
+      console.error(chalk`{magenta This is a dangerous operation that {bold may lead to data loss}.}`);
+      console.error(chalk`{magenta If you do experience data loss and you want to recover, run this first:}`);
+      console.error(chalk`{magenta {bold >} hub-sync -c ${cfg.what.user}/${cfg.what.repo}/hub-sync-recovery-${cfg.what.branch} ${old}}`);
+      console.error(chalk`{magenta and manually recover the data from the two branches.}`);
+    }
+    if (dryRun) {
+      console.error(chalk`Stopped because of {cyan --dry-run}`);
+      return true;
+    }
+    if (del) {
+      const res = await hs.delRefs(cfg.what);
+      console.error(chalk`{green Succeed:} {yellow ${show(cfg.what)}} has been {redBright {strikethrough deleted}}`);
+      debug(res);
+    } else {
+      const res = await hs.setRefs(cfg.what, sha, { force, create });
+      if (create) {
+        console.error(chalk`{green Succeed:} {yellow ${show(cfg.what)}} has been {green created}.`);
+      } else {
+        console.error(chalk`{green Succeed:} {yellow ${show(cfg.what)}} has been {cyan synchronized}.`);
+      }
+      debug(res);
+    }
+    return true;
+  } catch (e) {
+    if (!e) throw e;
+    if (!e.response) throw e;
+    if (!e.response.data) throw e;
+    if (e.response.data.message === 'Not Found') {
+      console.error(chalk`{red Failed: {italic ${e.response.data.message}.}} {dim Possible reasons:}`);
+      console.error(chalk`Case 1. You have not forked this repo yet.`);
+      console.error(chalk`    {dim Solution: Fork it on GitHub if you want. hub-sync takes care of synchronization, not initial forking.}`);
+      console.error(chalk`Case 2. You specified {yellow wrong branches} and/or repos.`);
+      console.error(chalk`    {dim Solution: Double check the command line and fix the error.}`);
+      console.error(chalk`Case 3. A branch has {yellow already been deleted}.`);
+      console.error(chalk`    {dim Solution: No need to worry.}`);
+    } else if (e.response.data.message === 'Reference does not exist') {
+      console.error(chalk`{red Failed: {italic ${e.response.data.message}.}} {dim Possible reasons:}`);
+      console.error(chalk`Case 1. You specified {yellow wrong branches} and/or repos.`);
+      console.error(chalk`    {dim Solution: Double check the command line and fix the error.}`);
+      console.error(chalk`Case 2. You want to re-create a branch that {yellow has been deleted} before.`);
+      console.error(chalk`    {dim Solution: Use {reset --create}.}`);
+      console.error(chalk`Case 3. The upstream repo developers {yellow created a new branch}.`);
+      console.error(chalk`    {dim Solution: Use {reset --create}.}`);
+    } else if (e.response.data.message === 'Reference already exists') {
+      console.error(chalk`{red Failed: {italic ${e.response.data.message}.}} {dim Possible reasons:}`);
+      console.error(chalk`Case 1. You specified {yellow wrong branches} and/or repos.`);
+      console.error(chalk`    {dim Solution: Double check the command line and fix the error.}`);
+      console.error(chalk`Case 2. You want to create a branch that has {yellow already been created}.`);
+      console.error(chalk`    {dim Solution: No need to worry.}`);
+      console.error(chalk`Case 3. You want to {yellow update} an exiting branch, not creating a new one.`);
+      console.error(chalk`    {dim Solution: Do {reset NOT} use {reset --create}.}`);
+    } else if (e.response.data.message === 'Update is not a fast forward') {
+      console.error(chalk`{red Failed: {italic ${e.response.data.message}.}} {dim Possible reasons:}`);
+      console.error(chalk`Case 1. You specified {yellow wrong branches} and/or repos.`);
+      console.error(chalk`    {dim Solution: Double check the command line and fix the error.}`);
+      console.error(chalk`Case 2. {yellow Your branch is ahead} of upstream (i.e. you have some changes that upstream doesn't).`);
+      console.error(chalk`    {dim Solution: hub-sync won't help in this case. Refer to this guide:}`);
+      console.error(chalk`    {dim {underline https://docs.github.com/en/github/collaborating-with-issues-and-pull-requests/syncing-a-fork}}`);
+      console.error(chalk`Case 3. The upstream repo developers {yellow force-pushed {italic their} branches}.`);
+      console.error(chalk`    {dim Solution: If you are 100% sure what you are doing, go for {reset --force}. Be extra extra careful in this case.}`);
+      console.error(chalk`    {dim You {reset will} lose data if the case is not handled carefully.}`);
+    } else {
+      throw e;
+    }
+    return false;
   }
 }
 
